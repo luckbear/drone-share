a73923b3cfa3fb2c3cd6c89a0368c5c8fa116a46
caixiaoguang <1023115946@qq.com>
2020-11-04 21:33:52 +0800

    init app

---
+++ app/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    develop: {
+        host: '127.0.0.1',
+        database: 'drone_db',
+        username: 'root',
+        password: '12345678',
+    },
+    production: {
+        host: '',
+        database: '',
+        username: '',
+        password: '',
+    },
+}
